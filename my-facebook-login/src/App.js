cc16417ac70e415e16186cb0946749ee7f478c90
Sanuj-T <sanuj@technmanconsulting.com>
2024-11-05 16:27:47 -0500

    jenkins first commit

---
+++ my-facebook-login/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here
+    alert("Login submitted!");
+  };
+
+  return (
+    <div className="container">
+      <h1>Facebook</h1>
+      <form onSubmit={handleSubmit} className="login-form">
+        <input type="text" placeholder="Email or Phone Number" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Log In</button>
+        <div className="links">
+          <a href="#">Forgotten password?</a>
+          <a href="#">Create New Account</a>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App; 
